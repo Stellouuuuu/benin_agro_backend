74af6f17c6efe365a877d5121709c304616877c2
Stellouuuuu <stella.gbaguidi@epitech.eu>
2025-10-28 10:16:50 +0100

    put the database online

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "benin_agro",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
